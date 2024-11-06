88f4ce1defcb02602a87bf00a6a934f0eeb539bc
Rituparna W <rituparnaw16@gmail.com>
2024-11-06 22:27:18 +0530

    dead

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="flex h-full items-center justify-center">
-    <div>Rituparna</div>
+  <div class="flex h-full items-center justify-center z-999">
+    <img src="/assets/img/logo.png" alt="rituparna" />
+    <client-only>
+      <vue-typer text="Hello World! I was registered locally!"></vue-typer
+    ></client-only>
   </div>
 </template>
+
+<script>
+import { VueTyper } from "vue-typer";
+
+
+</script>
