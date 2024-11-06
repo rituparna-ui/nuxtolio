8f120eb94f642f222220bd5105fd71197b21ff26
Rituparna W <rituparnaw16@gmail.com>
2024-11-06 19:01:55 +0530

    fix: add to attr to router-link

--- components/footer.vue
+++ components/footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center p-4 text-lg">
     Made with <font-awesome icon="heart" class="text-red-600" /> by
-    <router-link class="text-blue-600">Ritu</router-link>.
+    <router-link to="/" class="text-blue-600">Ritu</router-link>.
   </div>
 </template>
